6eb56373ac3f83ea2264154a0e34a26c1aa3f60b
zuhooruddin <zuhooruddin055@gmail.com>
2026-04-27 11:45:56 +0500

    resolve issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -175,7 +175,6 @@ const IndexPage = (props) => {
             <AdBanner
               slot={fluidSlot}
               format="fluid"
-              reserveSpace
               minHeight={{ xs: 120, sm: 140, md: 160 }}
               insStyle={{ minHeight: "120px" }}
               sx={{ maxWidth: 900, mx: "auto", mt: { xs: 2, sm: 3 } }}
